7f8021e23debec704d7d448e1ed2fb1266d6869b
Nicolai <nicolaibernhardchristensen@gmail.com>
2023-08-05 15:02:04 +0200

    get rid of dumb animations

--- components/hero-title.tsx
+++ components/hero-title.tsx
@@ -2,26 +2,18 @@
 
 import { bodoni_moda } from '@/lib/fonts';
 import { cn } from '@/lib/utils';
-import {
-  motion,
-  useTransform,
-  useMotionValueEvent,
-  useScroll,
-} from 'framer-motion';
 export default function HeroTitle({
   className,
   ...props
 }: React.HTMLAttributes<HTMLElement>) {
-  const { scrollYProgress } = useScroll();
-  const marginLeft = useTransform(scrollYProgress, [0, 1], ['5%', '35%']);
   return (
     <h1
       className={cn(
         bodoni_moda.className,
-        'break-all text-5xl sm:text-7xl lg:text-9xl w-full leading-[1.15] md:leading-[1.25] lg:leading-[1.35] uppercase',
+        'break-all text-5xl sm:text-7xl lg:text-9xl w-full leading-[1.2] md:leading-[1.25] lg:leading-[1.35] uppercase',
       )}
     >
-      <motion.span style={{ marginLeft }}>J</motion.span>
+      <span className={cn(`ml-[15%] md:ml-[35%]`)}>J</span>
       ust simple and elegant authentication in your hands
     </h1>
   );
